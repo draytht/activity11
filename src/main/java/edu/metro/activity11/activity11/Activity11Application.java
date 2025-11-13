2efcd2aec834a6edbc4d7dbf5158afb5302a1a00
THANH DAT <drayytran@gmail.com>
2025-11-13 15:38:27 -0600

    first commit

---
+++ src/main/java/edu/metro/activity11/activity11/Activity11Application.java
@@ -0,0 +1,13 @@
+package edu.metro.activity11.activity11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activity11Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/metro/activity11/activity11/Activity11ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.metro.activity11.activity11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
